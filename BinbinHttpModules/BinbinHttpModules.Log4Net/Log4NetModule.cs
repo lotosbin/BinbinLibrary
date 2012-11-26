0324418e845dba08741a4860f4092813a8ff06c4
lotosbin <lotosbin@gmail.com>
2012-11-26 15:14:11 +0800

    init binbin httpmodules

---
+++ BinbinHttpModules/BinbinHttpModules.Log4Net/Log4NetModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using log4net;
+
+namespace BinbinHttpModules.Log4Net
+{
+    public class Log4NetModule : IHttpModule
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.Error += Application_Error;
+        }
+
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = HttpContext.Current.Server.GetLastError().GetBaseException();
+            ILog log = LogManager.GetLogger(typeof (Log4NetModule));
+            log.Error("global", exception);
+        }
+    }
+}
