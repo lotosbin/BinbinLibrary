a3150c30c2b64f5d85653cc7cbfe5f20136efc91
liubinbin <lotosbin@gmail.com>
2015-12-21 15:14:54 +0800

    split projects

--- ClassLibraries/Binbin.TextTemplate/TextTemplateTranformer.cs
+++ ClassLibraries/Binbin.TextTemplate/TextTemplateTranformer.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace Binbin.TextTemplate
-{
-    public class TextTemplateTranformer
-    {
-        public static string TransformTemplate(string bodyTemplate, List<KeyValuePair<string, string>> bargs)
-        {
-            string mailBody = bodyTemplate;
-            foreach (var arg in bargs)
-            {
-                mailBody = mailBody.Replace("{{" + arg.Key + "}}", arg.Value);
-
-            }
-            return mailBody;
+namespace Binbin.TextTemplate {
+    public class TextTemplateTranformer {
+        public static string TransformTemplate(string bodyTemplate, List<KeyValuePair<string, string>> bargs) {
+            return bargs.Aggregate(bodyTemplate, (current, arg) => current.Replace("{{" + arg.Key + "}}", arg.Value));
         }
     }
 }
--- ClassLibraries/BinbinWpfUnityApplication/WpfUnityApplication.cs
+++ ClassLibraries/BinbinWpfUnityApplication/WpfUnityApplication.cs
@@ -1,32 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Practices.Unity;
 using System.Windows;
-using Microsoft.Practices.Unity;
 
-namespace BinbinWpfUnityApplication
-{
-    public class WpfUnityApplication : Application
-    {
-        protected override void OnStartup(StartupEventArgs e)
-        {
+namespace BinbinWpfUnityApplication {
+    public class WpfUnityApplication : Application {
+        protected override void OnStartup(StartupEventArgs e) {
             base.OnStartup(e);
             var unityContainer = new UnityContainer();
             this.Properties["UnityContainer"] = unityContainer;
             RegisterTypes(unityContainer);
         }
-        public static IUnityContainer UnityContainer
-        {
-            get
-            {
-                return (IUnityContainer)Application.Current.Properties["UnityContainer"];
-            }
-        }
+        public static IUnityContainer UnityContainer => (IUnityContainer)Application.Current.Properties["UnityContainer"];
 
-        protected virtual void RegisterTypes(IUnityContainer unityContainer)
-        {
+        protected virtual void RegisterTypes(IUnityContainer unityContainer) {
 
         }
     }
